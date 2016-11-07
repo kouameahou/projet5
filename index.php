ea2f667d81bed226b33524c67f579302efc70cf0
chantalkouame <chantalkouame007@gmail.com>
2016-11-07 09:26:59 +0000

    création de ma page d'accueil

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>ma première version</title>
+	<meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<h1> I - INITIATION AU VERSIONING (Git)</h1><hr>
+		<p>Bonjour tout le monde! Bienvenue sur ma première version</p>
+	</div>
+</body>
+</html>
